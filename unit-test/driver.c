17ed20da160430bcc9cd604aeb0cfcf6b1f69e39
Pietro Cerutti <gahr@gahr.ch>
2017-02-24 11:42:41 +0000

    Test push/front/pop

--- unit-test/driver.c
+++ unit-test/driver.c
@@ -11,4 +11,5 @@
 TEST_LIST = {{"list_create", test_list_create},
              {"list_add", test_list_add},
              {"list_find", test_list_find},
+             {"list_push_pop", test_list_push_pop},
              {0}};
--- unit-test/test_list.c
+++ unit-test/test_list.c
@@ -5,6 +5,8 @@ void test_list_create()
 {
   struct List *l = mutt_new_list();
   TEST_CHECK(l != NULL);
+  mutt_free_list(&l);
+  TEST_CHECK(l == NULL);
 }
 
 void test_list_add()
@@ -12,6 +14,7 @@ void test_list_add()
   struct List *l1 = mutt_new_list();
   struct List *l2 = mutt_add_list(l1, "Hello");
   TEST_CHECK(l1 == l2);
+  mutt_free_list(&l1);
 }
 
 void test_list_find()
@@ -24,4 +27,16 @@ void test_list_find()
   l2 = mutt_find_list(l1, "Hello");
   TEST_CHECK(l1 != l2);
   TEST_CHECK(strcmp(l2->data, "Hello") == 0);
+  mutt_free_list(&l1);
+}
+
+void test_list_push_pop()
+{
+  struct List *l = mutt_new_list();
+  mutt_push_list(&l, "Hello");
+  mutt_push_list(&l, "World");
+  TEST_CHECK(strcmp(mutt_front_list(l), "World") == 0);
+  mutt_pop_list(&l);
+  TEST_CHECK(strcmp(mutt_front_list(l), "Hello") == 0);
+  mutt_free_list(&l);
 }
