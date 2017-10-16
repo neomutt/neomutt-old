mb720/neomutt

0005-debian-specific-Md.etc_mailname_gethostbyname

init.c
@@ -51,6 +51,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/utsname.h>
+#include <netdb.h>
 #include <errno.h>
 #include <sys/wait.h>
 #include <sys/time.h>
@@ -3590,6 +3591,31 @@ static int mutt_execute_commands (LIST *p)
   return 0;
 }
 
+int getmailname(char *s, size_t l)
+{
+    FILE *f;
+    char tmp[512];
+    char *p = tmp;
+
+    if ((f = fopen ("/etc/mailname", "r")) == NULL)
+       return (-1);
+
+    if (fgets (tmp, 510, f) != NULL) {
+      while (*p && !ISSPACE(*p) && l > 0) {
+	*s++ = *p++;
+	l--;
+      }
+      if (*(s-1) == '.')
+	s--;
+      *s = 0;
+
+      fclose (f);
+      return 0;
+    }
+    fclose (f);
+    return (-1);
+}
+
 static char* mutt_find_cfg (const char *home, const char *xdg_cfg_home)
 {
   const char* names[] =
@@ -3726,8 +3752,10 @@ void mutt_init (int skip_sys_rc, LIST *commands)
   else
     Hostname = safe_strdup (utsname.nodename);
 
-  /* now get FQDN.  Use configured domain first, DNS next, then uname */
-  if (domain)
+  /* now get FQDN.  Use /etc/mailname first, then configured domain, DNS next, then uname */
+  if (getmailname(buffer, sizeof (buffer)) != -1)
+    Fqdn = safe_strdup(buffer);
+  else if (domain)
   {
     /* we have a compile-time domain name, use that for Fqdn */
     Fqdn = safe_malloc (mutt_strlen (domain) + mutt_strlen (Hostname) + 2);

neomutt/neomutt-old

init.c
@@ -23,6 +23,7 @@
 #include "config.h"
 #include <ctype.h>
 #include <errno.h>
+#include <netdb.h>
 #include <fcntl.h>
 #include <inttypes.h>
 #include <libgen.h>
@@ -4049,6 +4050,33 @@ static char *find_cfg(const char *home, const char *xdg_cfg_home)
   return NULL;
 }
 
+int getmailname(char *s, size_t l)
+{
+  FILE *f;
+  char tmp[512];
+  char *p = tmp;
+
+  if ((f = fopen("/etc/mailname", "r")) == NULL)
+    return (-1);
+
+  if (fgets(tmp, 510, f) != NULL)
+  {
+    while (*p && !ISSPACE(*p) && l > 0)
+    {
+      *s++ = *p++;
+      l--;
+    }
+    if (*(s - 1) == '.')
+      s--;
+    *s = 0;
+
+    fclose(f);
+    return 0;
+  }
+  fclose(f);
+  return (-1);
+}
+
 void mutt_init(int skip_sys_rc, struct ListHead *commands)
 {
   struct passwd *pw = NULL;
@@ -4150,11 +4178,17 @@ void mutt_init(int skip_sys_rc, struct ListHead *commands)
   else
     ShortHostname = safe_strdup(utsname.nodename);
 
-/* now get FQDN.  Use configured domain first, DNS next, then uname */
+  /* now get FQDN.  Use /etc/mailname first, then configured domain, DNS next, then uname */
+  if (getmailname(buffer, sizeof(buffer)) != -1)
+    Hostname = safe_strdup(buffer);
 #ifdef DOMAIN
   /* we have a compile-time domain name, use that for Hostname */
-  Hostname = safe_malloc(mutt_strlen(DOMAIN) + mutt_strlen(ShortHostname) + 2);
-  sprintf(Hostname, "%s.%s", NONULL(ShortHostname), DOMAIN);
+  if (!Hostname)
+  {
+    Hostname = safe_malloc(mutt_strlen(DOMAIN) + mutt_strlen(Hostname) + 2);
+    sprintf(Hostname, "%s.%s", NONULL(Hostname), DOMAIN); /* __SPRINTF_CHECKED__ */
+  }
+  else
 #else
   if (!(getdnsdomainname(buffer, sizeof(buffer))))
   {