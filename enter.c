2bc972633758dacb1be4406b2d30f91aace0c9f4
Pablo Ovelleiro Corral <pablo1@mailbox.org>
2018-12-29 12:47:30 +0100

    catch esc-keypresses in the command prompt

--- enter.c
+++ enter.c
@@ -769,6 +769,12 @@ int mutt_enter_string_full(char *buf, size_t buflen, int col,
         }
       }
 
+      /* Close prompt with escape key */
+      if (wc == '\033')
+      {
+        goto bye;
+      }
+
       if ((wc == '\r') || (wc == '\n'))
       {
         /* Convert from wide characters */
