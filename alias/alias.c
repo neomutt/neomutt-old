e061c13af1cf0fff43d458e71b27bd7118d5349a
Richard Russon <rich@flatcap.org>
2020-03-31 18:23:01 +0100

    WIP pretty

--- alias/alias.c
+++ alias/alias.c
@@ -485,6 +485,12 @@ retry_name:
   const char *alias_file = cs_subset_path(sub, "alias_file");
   mutt_str_copy(buf, NONULL(alias_file), sizeof(buf));
 
+  char *pretty = NULL;
+  if (mutt_path2_pretty(buf, HomeDir, &pretty))
+  {
+    mutt_str_copy(buf, pretty, sizeof(buf));
+    FREE(&pretty);
+  }
   if (mutt_get_field(_("Save to file: "), buf, sizeof(buf), MUTT_FILE | MUTT_CLEAR) != 0)
     return;
   mutt_expand_path(buf, sizeof(buf));
