neomutt/neomutt

imap: retrieve bodystructure when mailbox is opened
Currently %X in index_format is unusable because it downloads
all messages.

This change implements BODYSTRUCTURE parsing from rfc3501.

The message content is set with this structure avoiding
mutt_parse_mime_message() to call mx_open_message for imap.

%X is no more need to download message to be calculated.

mutt_parse.c
@@ -32,7 +32,10 @@
 #include <stdio.h>
 #include "mutt/lib.h"
 #include "email/lib.h"
+#include "core/lib.h"
 #include "mutt_parse.h"
+#include "context.h"
+#include "globals.h"
 #include "mx.h"
 #include "ncrypt/lib.h"
 
@@ -53,7 +56,9 @@ void mutt_parse_mime_message(struct Mailbox *m, struct Email *e)
     if ((e->content->type != TYPE_MESSAGE) && (e->content->type != TYPE_MULTIPART))
       break; /* nothing to do */
 
-    if (e->content->parts)
+    /* FIXME(sileht): We read the message for getting hdr_offset and offset
+     * for imap */
+    if (e->content->parts && (m->magic != MUTT_IMAP))
       break; /* The message was parsed earlier. */
 
     struct Message *msg = mx_msg_open(m, e->msgno);

neomutt/neomutt-old

mutt_parse.c
@@ -32,7 +32,10 @@
 #include <stdio.h>
 #include "mutt/lib.h"
 #include "email/lib.h"
+#include "core/lib.h"
 #include "mutt_parse.h"
+#include "context.h"
+#include "globals.h"
 #include "mx.h"
 #include "ncrypt/lib.h"
 
@@ -53,7 +56,9 @@ void mutt_parse_mime_message(struct Mailbox *m, struct Email *e)
     if ((e->content->type != TYPE_MESSAGE) && (e->content->type != TYPE_MULTIPART))
       break; /* nothing to do */
 
-    if (e->content->parts)
+    /* FIXME(sileht): We read the message for getting hdr_offset and offset
+     * for imap */
+    if (e->content->parts && (m->type != MUTT_IMAP))
       break; /* The message was parsed earlier. */
 
     struct Message *msg = mx_msg_open(m, e->msgno);