reedts/neomutt

refactor: move C_Folder out of mx.c
These functions don't have an Account:
- `mx_mbox_find2()`
- `mx_path_resolve()`

This means that in the future, they wouldn't be able to lookup the
correct value for `$folder`.

autocrypt/autocrypt.c
@@ -917,7 +917,7 @@ void mutt_autocrypt_scan_mailboxes(void)
         (!mutt_buffer_is_empty(folderbuf)))
     {
       mutt_buffer_expand_path_regex(folderbuf, false);
-      struct Mailbox *m = mx_path_resolve(mutt_b2s(folderbuf));
+      struct Mailbox *m = mx_path_resolve(mutt_b2s(folderbuf), C_Folder);
       /* NOTE: I am purposely *not* executing folder hooks here,
        * as they can do all sorts of things like push into the getch() buffer.
        * Authentication should be in account-hooks. */

compose.c
@@ -1723,7 +1723,7 @@ int mutt_compose_menu(struct Email *e, struct Buffer *fcc, struct Email *e_cur, 
 
         menu->redraw = REDRAW_FULL;
 
-        struct Mailbox *m = mx_path_resolve(mutt_b2s(&fname));
+        struct Mailbox *m = mx_path_resolve(mutt_b2s(&fname), C_Folder);
         bool old_readonly = m->readonly;
         struct Context *ctx = mx_mbox_open(m, MUTT_READONLY);
         if (!ctx)

editmsg.c
@@ -69,7 +69,7 @@ static int ev_message(enum EvMessage action, struct Mailbox *m, struct Email *e)
   enum MailboxType omagic = C_MboxType;
   C_MboxType = MUTT_MBOX;
 
-  struct Mailbox *m_fname = mx_path_resolve(mutt_b2s(fname));
+  struct Mailbox *m_fname = mx_path_resolve(mutt_b2s(fname), C_Folder);
   struct Context *ctx_tmp = mx_mbox_open(m_fname, MUTT_NEWFOLDER);
 
   C_MboxType = omagic;

mx.c
@@ -541,7 +541,7 @@ static int trash_append(struct Mailbox *m)
   }
 #endif
 
-  struct Mailbox *m_trash = mx_path_resolve(C_Trash);
+  struct Mailbox *m_trash = mx_path_resolve(C_Trash, C_Folder);
   const bool old_append = m_trash->append;
   struct Context *ctx_trash = mx_mbox_open(m_trash, MUTT_APPEND);
   if (ctx_trash)
@@ -746,7 +746,7 @@ int mx_mbox_close(struct Context **ptr)
     else /* use regular append-copy mode */
 #endif
     {
-      struct Mailbox *m_read = mx_path_resolve(mutt_b2s(mbox));
+      struct Mailbox *m_read = mx_path_resolve(mutt_b2s(mbox), C_Folder);
       struct Context *ctx_read = mx_mbox_open(m_read, MUTT_APPEND);
       if (!ctx_read)
       {
@@ -1614,18 +1614,19 @@ done:
 
 /**
  * mx_mbox_find2 - Find a Mailbox on an Account
- * @param path Path to find
+ * @param path   Path to find
+ * @param folder Root mailbox path
  * @retval ptr  Mailbox
  * @retval NULL No match
  */
-struct Mailbox *mx_mbox_find2(const char *path)
+struct Mailbox *mx_mbox_find2(const char *path, const char *folder)
 {
   if (!path)
     return NULL;
 
   char buf[PATH_MAX];
   mutt_str_strfcpy(buf, path, sizeof(buf));
-  mx_path_canon(buf, sizeof(buf), C_Folder, NULL);
+  mx_path_canon(buf, sizeof(buf), folder, NULL);
 
   struct Account *np = NULL;
   TAILQ_FOREACH(np, &NeoMutt->accounts, entries)
@@ -1640,24 +1641,25 @@ struct Mailbox *mx_mbox_find2(const char *path)
 
 /**
  * mx_path_resolve - Get a Mailbox for a path
- * @param path Mailbox path
+ * @param path   Mailbox path
+ * @param folder Root mailbox path
  * @retval ptr Mailbox
  *
  * If there isn't a Mailbox for the path, one will be created.
  */
-struct Mailbox *mx_path_resolve(const char *path)
+struct Mailbox *mx_path_resolve(const char *path, const char *folder)
 {
   if (!path)
     return NULL;
 
-  struct Mailbox *m = mx_mbox_find2(path);
+  struct Mailbox *m = mx_mbox_find2(path, folder);
   if (m)
     return m;
 
   m = mailbox_new(NULL);
   m->flags = MB_HIDDEN;
   mutt_str_replace(&m->path->orig, path);
-  mx_path_canon2(m, C_Folder);
+  mx_path_canon2(m, folder);
 
   return m;
 }

neomutt/neomutt-old

autocrypt/autocrypt.c
@@ -920,7 +920,7 @@ void mutt_autocrypt_scan_mailboxes(void)
         (!mutt_buffer_is_empty(folderbuf)))
     {
       mutt_buffer_expand_path_regex(folderbuf, false);
-      struct Mailbox *m = mx_path_resolve(mutt_buffer_string(folderbuf));
+      struct Mailbox *m = mx_path_resolve(mutt_buffer_string(folderbuf), C_Folder);
       /* NOTE: I am purposely *not* executing folder hooks here,
        * as they can do all sorts of things like push into the getch() buffer.
        * Authentication should be in account-hooks. */

compose/compose.c
@@ -2115,7 +2115,7 @@ int mutt_compose_menu(struct Email *e, struct Buffer *fcc, struct Email *e_cur,
 
         menu->redraw = REDRAW_FULL;
 
-        struct Mailbox *m = mx_path_resolve(mutt_buffer_string(&fname));
+        struct Mailbox *m = mx_path_resolve(mutt_buffer_string(&fname), C_Folder);
         bool old_readonly = m->readonly;
         struct Context *ctx = mx_mbox_open(m, MUTT_READONLY);
         if (!ctx)

editmsg.c
@@ -69,7 +69,7 @@ static int ev_message(enum EvMessage action, struct Mailbox *m, struct Email *e)
   enum MailboxType otype = C_MboxType;
   C_MboxType = MUTT_MBOX;
 
-  struct Mailbox *m_fname = mx_path_resolve(mutt_buffer_string(fname));
+  struct Mailbox *m_fname = mx_path_resolve(mutt_buffer_string(fname), C_Folder);
   struct Context *ctx_tmp = mx_mbox_open(m_fname, MUTT_NEWFOLDER);
 
   C_MboxType = otype;

mx.c
@@ -572,7 +572,7 @@ static int trash_append(struct Mailbox *m)
   }
 #endif
 
-  struct Mailbox *m_trash = mx_path_resolve(C_Trash);
+  struct Mailbox *m_trash = mx_path_resolve(C_Trash, C_Folder);
   const bool old_append = m_trash->append;
   struct Context *ctx_trash = mx_mbox_open(m_trash, MUTT_APPEND);
   if (ctx_trash)
@@ -782,7 +782,7 @@ enum MxStatus mx_mbox_close(struct Context **ptr)
     else /* use regular append-copy mode */
 #endif
     {
-      struct Mailbox *m_read = mx_path_resolve(mutt_buffer_string(mbox));
+      struct Mailbox *m_read = mx_path_resolve(mutt_buffer_string(mbox), C_Folder);
       struct Context *ctx_read = mx_mbox_open(m_read, MUTT_APPEND);
       if (!ctx_read)
       {
@@ -1642,18 +1642,19 @@ done:
 
 /**
  * mx_mbox_find2 - Find a Mailbox on an Account
- * @param path Path to find
+ * @param path   Path to find
+ * @param folder Root mailbox path
  * @retval ptr  Mailbox
  * @retval NULL No match
  */
-struct Mailbox *mx_mbox_find2(const char *path)
+struct Mailbox *mx_mbox_find2(const char *path, const char *folder)
 {
   if (!path)
     return NULL;
 
   char buf[PATH_MAX];
   mutt_str_copy(buf, path, sizeof(buf));
-  mx_path_canon(buf, sizeof(buf), C_Folder, NULL);
+  mx_path_canon(buf, sizeof(buf), folder, NULL);
 
   struct Account *np = NULL;
   TAILQ_FOREACH(np, &NeoMutt->accounts, entries)
@@ -1668,24 +1669,25 @@ struct Mailbox *mx_mbox_find2(const char *path)
 
 /**
  * mx_path_resolve - Get a Mailbox for a path
- * @param path Mailbox path
+ * @param path   Mailbox path
+ * @param folder Root mailbox path
  * @retval ptr Mailbox
  *
  * If there isn't a Mailbox for the path, one will be created.
  */
-struct Mailbox *mx_path_resolve(const char *path)
+struct Mailbox *mx_path_resolve(const char *path, const char *folder)
 {
   if (!path)
     return NULL;
 
-  struct Mailbox *m = mx_mbox_find2(path);
+  struct Mailbox *m = mx_mbox_find2(path, folder);
   if (m)
     return m;
 
   m = mailbox_new(NULL);
   m->flags = MB_HIDDEN;
   mutt_str_replace(&m->path->orig, path);
-  mx_path_canon2(m, C_Folder);
+  mx_path_canon2(m, folder);
 
   return m;
 }
@@ -1752,7 +1754,7 @@ struct Mailbox *mx_resolve(const char *path_or_name)
   // a new Mailbox for us.
   struct Mailbox *m = mx_mbox_find_by_name(path_or_name);
   if (!m)
-    m = mx_path_resolve(path_or_name);
+    m = mx_path_resolve(path_or_name, C_Folder);
 
   return m;
 }