reedts/neomutt

WIP eliminate pretty

enter.c
@@ -651,6 +651,14 @@ int mutt_enter_string_full(char *buf, size_t buflen, int col,
                                files, numfiles);
               if (buf[0] != '\0')
               {
+                char *pretty = NULL;
+                mutt_path2_pretty(buf, HomeDir, &pretty);
+                if (pretty)
+                {
+                  mutt_str_strfcpy(buf, pretty, buflen);
+                  FREE(&pretty);
+                }
+                //JKJ this could be a file/dir or a mailbox!
                 mutt_pretty_mailbox(buf, buflen);
                 if (!pass)
                   mutt_hist_add(hclass, buf, true);

main.c
@@ -723,14 +723,10 @@ int main(int argc, char *argv[], char *envp[])
 
     mutt_buffer_strcpy(fpath, C_Folder);
     mutt_buffer_expand_path(fpath);
-    bool skip = false;
-#ifdef USE_IMAP
-    /* we're not connected yet - skip mail folder creation */
-    skip |= (imap_path_probe(mutt_b2s(fpath), NULL) == MUTT_IMAP);
-#endif
-#ifdef USE_NNTP
-    skip |= (nntp_path_probe(mutt_b2s(fpath), NULL) == MUTT_NNTP);
-#endif
+    enum MailboxType type = mx_path_probe(mutt_b2s(fpath));
+    //QWQ use MxOps.is_local
+    //QWQ or drop create completely
+    bool skip = (type == MUTT_IMAP) || (type == MUTT_NNTP) || (type == MUTT_NOTMUCH);
     if (!skip && (stat(mutt_b2s(fpath), &sb) == -1) && (errno == ENOENT))
     {
       char msg2[256];

neomutt/neomutt-old

enter.c
@@ -671,6 +671,14 @@ int mutt_enter_string_full(char *buf, size_t buflen, int col,
                                files, numfiles);
               if (buf[0] != '\0')
               {
+                char *pretty = NULL;
+                mutt_path2_pretty(buf, HomeDir, &pretty);
+                if (pretty)
+                {
+                  mutt_str_copy(buf, pretty, buflen);
+                  FREE(&pretty);
+                }
+                //JKJ this could be a file/dir or a mailbox!
                 mutt_pretty_mailbox(buf, buflen);
                 if (!pass)
                   mutt_hist_add(hclass, buf, true);

main.c
@@ -790,17 +790,10 @@ int main(int argc, char *argv[], char *envp[])
 
     mutt_buffer_strcpy(fpath, C_Folder);
     mutt_buffer_expand_path(fpath);
-    bool skip = false;
-#ifdef USE_IMAP
-    /* we're not connected yet - skip mail folder creation */
-    skip |= (imap_path_probe(mutt_buffer_string(fpath), NULL) == MUTT_IMAP);
-#endif
-#ifdef USE_POP
-    skip |= (pop_path_probe(mutt_buffer_string(fpath), NULL) == MUTT_POP);
-#endif
-#ifdef USE_NNTP
-    skip |= (nntp_path_probe(mutt_buffer_string(fpath), NULL) == MUTT_NNTP);
-#endif
+    enum MailboxType type = mx_path_probe(mutt_buffer_string(fpath));
+    //QWQ use MxOps.is_local
+    //QWQ or drop create completely
+    bool skip = (type == MUTT_IMAP) || (type == MUTT_NNTP) || (type == MUTT_NOTMUCH);
     if (!skip && (stat(mutt_buffer_string(fpath), &sb) == -1) && (errno == ENOENT))
     {
       char msg2[256];