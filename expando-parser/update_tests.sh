cd0ca2d705c4aeebd05879453398f192ae01366d
Tóth János <gomba007@gmail.com>
2023-07-24 11:00:59 +0200

    parser: Add tests.
    Tests can be run using `make test`.
    The tests are checked against a known-to-be-good output. This 'golden
    data' can be (re)generated using `./update_tests.sh` then checked by hand.

---
+++ expando-parser/update_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf tests/*.checked
+
+ALL=0
+for i in tests/*.in; do
+    CHECKED=$(echo "$i" | sed s/in/checked/)
+    ./parser "$(head -n 1 $i)" > "$CHECKED"
+    ALL=$((ALL+1))
+done
+
+echo "$ALL tests updated"
