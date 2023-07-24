163484b613ed88f9561cfccbcbcc77b4c5e6ac99
Tóth János <gomba007@gmail.com>
2023-07-24 11:23:52 +0200

    parser: Fix test output generation.

--- expando-parser/test_parser.c
+++ expando-parser/test_parser.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
   for (int i = 1; i < argc; i++)
   {
     const char *text = argv[i];
-    printf("%s\n", text);
+    printf("`%s`\n", text);
 
     struct Node *root = NULL;
     parse_tree(&root, text);
