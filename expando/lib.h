155fc6dec33fb9bedaed50143ff26c9e68a0634a
Richard Russon <rich@flatcap.org>
2023-08-11 00:35:47 +0100

    doxy: link up source/library

--- expando/lib.h
+++ expando/lib.h
@@ -1,3 +1,41 @@
+/**
+ * @file
+ * Parse expando string
+ *
+ * @authors
+ * Copyright (C) 2023 Tóth János <gomba007@gmail.com>
+ *
+ * @copyright
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @page lib_expando Parse expando string
+ *
+ * Parse expando string
+ *
+ * | File                             | Description                   |
+ * | :------------------------------- | :---------------------------- |
+ * | expando/format_callbacks.c       | @subpage expando_format       |
+ * | expando/global_table.c           | @subpage expando_global       |
+ * | expando/helpers.c                | @subpage expando_helpers      |
+ * | expando/index_format_callbacks.c | @subpage expando_index_format |
+ * | expando/node.c                   | @subpage expando_node         |
+ * | expando/parser.c                 | @subpage expando_parser       |
+ * | expando/validation.c             | @subpage expando_validation   |
+ */
+
 #ifndef MUTT_EXPANDO_LIB_H
 #define MUTT_EXPANDO_LIB_H
 
